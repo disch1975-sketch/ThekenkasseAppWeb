37bec8b43aa73911f353eba0d2dabb537f7183ed
Dirk Schäfer <disch1975@gmail.com>
2026-05-11 08:52:37 +0200

    Release …

---
+++ barcode.js
@@ -0,0 +1,47 @@
+﻿window.barcodeScanner = (function () {
+
+    let buffer = "";
+    let dotNetRef = null;
+
+    function init(dotNetObject) {
+        dotNetRef = dotNetObject;
+
+        document.addEventListener("keydown", handleKeyDown);
+    }
+
+    function handleKeyDown(e) {
+
+        // Enter = Scan abschließen
+        if (e.key === "Enter") {
+
+            if (buffer.length > 0 && dotNetRef) {
+                dotNetRef.invokeMethodAsync("HandleScanGlobal", buffer);
+            }
+
+            buffer = "";
+            return;
+        }
+
+        // nur echte Zeichen sammeln
+        if (e.key.length === 1) {
+            buffer += e.key;
+        }
+
+        // optional: Reset bei Escape
+        if (e.key === "Escape") {
+            buffer = "";
+        }
+    }
+
+    function dispose() {
+        document.removeEventListener("keydown", handleKeyDown);
+        buffer = "";
+        dotNetRef = null;
+    }
+
+    return {
+        init,
+        dispose
+    };
+
+})();
